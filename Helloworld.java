08752c0d31be76602b5fa452ac3c09a3bd65e76b
amlas lui <amlas2010@live.fr>
2019-04-29 23:51:05 +0200

    commit 1....

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+public class Helloworld{
+public static void main (String[] args) {
+	System.out.println("Salam...............");
+
+	System.out.println("Salam aalikoum..............");
+
+	System.out.println("Salam salam...............");
+}
+
+
+
+}
