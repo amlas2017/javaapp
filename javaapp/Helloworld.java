1b78ad04929af6a4578132cbc4a6ba2c883dd503
amlas2017 <amlas2010@live.fr>
2019-04-30 09:57:24 +0000

    déplacer le fichier Helloworld.java dans javaapp

---
+++ javaapp/Helloworld.java
@@ -0,0 +1,12 @@
+public class Helloworld{
+public static void main (String[] args) {
+	System.out.println("Salam...............");
+
+	System.out.println("Salam aalikoum..............");
+
+	System.out.println("On est le 30/04/2019...............");
+}
+
+
+
+}
