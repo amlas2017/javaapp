e6a9262147c6e3d8e660248c14b5e11c1b584b42
amlas2017 <amlas2010@live.fr>
2019-04-30 10:31:18 +0000

    2de version de l'image docker : javaapp:v2

--- javaapp/Helloworld.java
+++ javaapp/Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld{
 public static void main (String[] args) {
 	System.out.println("Salam...............");
 
-	System.out.println("Salam aalikoum..............");
+	System.out.println("Ce programme java est exécuté dans un container Docker construit à partir d'un Dockerfile......");
 
 	System.out.println("On est le 30/04/2019...............");
 }
